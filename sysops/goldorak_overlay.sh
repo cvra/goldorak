3db3fa3c2a39e3b95dfd0c39ed6e93fa2bdbfb75
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2015-10-12 22:44:47 +0200

    Fix CAN interface configuration

--- sysops/goldorak_overlay.sh
+++ sysops/goldorak_overlay.sh
@@ -7,5 +7,5 @@ sudo config-pin P9.24 can
 sudo config-pin P9.26 can
 
 # SocketCAN
-sudo ip link set can1 up type can bitrate 1000000
+sudo ip link set can1 up type can bitrate 1000000 sample-point 0.875 restart-ms 100
 sudo ifconfig can1 up
