e3a7d08e2cf65b6ede85c279233d5f9a2d01d58b
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2015-10-15 18:26:19 +0200

    Split bridge into class dedicated to controlling motors over UAVCAN

--- uavcan_core/src/uavcan_bridge.cpp
+++ uavcan_core/src/uavcan_bridge.cpp
@@ -18,15 +18,49 @@ constexpr unsigned NodeMemoryPoolSize = 16384;
 typedef uavcan::Node<NodeMemoryPoolSize> Node;
 
 
+class UavcanRosMotorController {
+public:
+    ros::Subscriber ros_velocity_sub;
+    uavcan::Publisher<cvra::motor::control::Velocity> uc_velocity_pub;
+    cvra::motor::control::Velocity uc_velocity_msg;
+
+    UavcanRosMotorController(Node& uc_node, ros::NodeHandle& ros_node):
+        uc_velocity_pub(uc_node)
+    {
+        /* ROS subscribers */
+        ros_velocity_sub = ros_node.subscribe(
+            "vel_commands", 10, &UavcanRosMotorController::velocityCallback, this);
+
+        /* UAVCAN publishers */
+        const int vel_pub_init_res = uc_velocity_pub.init();
+        if (vel_pub_init_res < 0) {
+            throw std::runtime_error("Failed to start the publisher");
+        }
+        uc_velocity_pub.setTxTimeout(uavcan::MonotonicDuration::fromMSec(1000));
+        uc_velocity_pub.setPriority(uavcan::TransferPriority::MiddleLower);
+    }
+
+    void velocityCallback(const cvra_msgs::MotorControlVelocity::ConstPtr& msg)
+    {
+        uc_velocity_msg.velocity = msg->velocity;
+        uc_velocity_msg.node_id = msg->node_id;
+
+        ROS_INFO("I heard: [%f]", msg->velocity);
+
+        const int pub_res = uc_velocity_pub.broadcast(uc_velocity_msg);
+        if (pub_res < 0) {
+            std::cerr << "Vel publication failure: " << pub_res << std::endl;
+        }
+    }
+};
+
 class UavcanRosBridge {
 public:
     int node_id;
     Node uc_node;
     ros::NodeHandle ros_node;
 
-    ros::Subscriber velocity_ros_sub;
-    cvra::motor::control::Velocity velocity_sub_msg;
-    uavcan::Publisher<cvra::motor::control::Velocity>* velocity_uc_pub;
+    UavcanRosMotorController motor_controller;
 
     ros::Publisher encoder_ros_pub;
     cvra_msgs::MotorFeedbackEncoderPosition encoder_pub_msg;
@@ -35,14 +69,12 @@ public:
     uavcan::Subscriber<uavcan::protocol::debug::LogMessage>* log_uavcan_sub;
 
     UavcanRosBridge(int id):
-        uc_node(getCanDriver(), getSystemClock())
+        uc_node(getCanDriver(), getSystemClock()),
+        motor_controller(uc_node, ros_node)
     {
         node_id = id;
 
         /* Start ROS subscribers / publishers */
-        velocity_ros_sub = ros_node.subscribe(
-            "vel_commands", 10, &UavcanRosBridge::velocityCallback, this);
-
         encoder_ros_pub = ros_node.advertise<cvra_msgs::MotorFeedbackEncoderPosition>(
             "encoder_raw", 10);
 
@@ -55,14 +87,6 @@ public:
         if (node_start_res < 0) {
             throw std::runtime_error("Failed to start the node");
         }
-
-        velocity_uc_pub = new uavcan::Publisher<cvra::motor::control::Velocity>(uc_node);
-        const int vel_pub_init_res = velocity_uc_pub->init();
-        if (vel_pub_init_res < 0) {
-            throw std::runtime_error("Failed to start the publisher");
-        }
-        velocity_uc_pub->setTxTimeout(uavcan::MonotonicDuration::fromMSec(1000));
-        velocity_uc_pub->setPriority(uavcan::TransferPriority::MiddleLower);
 
         log_uavcan_sub = new uavcan::Subscriber<uavcan::protocol::debug::LogMessage>(uc_node);
         const int log_sub_start_res = log_uavcan_sub->start(
@@ -91,20 +115,6 @@ public:
         }
 
         uc_node.setModeOperational();
-
-    }
-
-    void velocityCallback(const cvra_msgs::MotorControlVelocity::ConstPtr& msg)
-    {
-        ROS_INFO("I heard: [%f]", msg->velocity);
-
-        velocity_sub_msg.velocity = msg->velocity;
-        velocity_sub_msg.node_id = msg->node_id;
-
-        const int pub_res = velocity_uc_pub->broadcast(velocity_sub_msg);
-        if (pub_res < 0) {
-            std::cerr << "Vel publication failure: " << pub_res << std::endl;
-        }
     }
 
     void spin(void)
