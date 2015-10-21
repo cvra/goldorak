fb15caf788fe058d18ab60b13c2de2599dd46b35
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2015-10-21 20:44:29 +0200

    Implement first prototype of motor configuration

--- uavcan_core/src/motor_config_server.cpp
+++ uavcan_core/src/motor_config_server.cpp
@@ -1,26 +1,28 @@
 #include <ros/ros.h>
 
 #include <dynamic_reconfigure/server.h>
-#include <dynamic_tutorials/TutorialsConfig.h>
+#include <uavcan_core/MotorBoardConfig.h>
 
-void callback(dynamic_tutorials::TutorialsConfig &config, uint32_t level) {
-  ROS_INFO("Reconfigure Request: %d %f %s %s %d",
-            config.int_param, config.double_param,
-            config.str_param.c_str(),
-            config.bool_param?"True":"False",
-            config.size);
+void callback(uavcan_core::MotorBoardConfig &config, uint32_t level)
+{
+    ROS_INFO("Reconfigure Request: %.3f %.3f %.3f %.3f",
+             config.position_p,
+             config.position_i,
+             config.position_d,
+             config.position_i_limit);
 }
 
-int main(int argc, char **argv) {
-  ros::init(argc, argv, "dynamic_tutorials");
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "motor_config_server");
 
-  dynamic_reconfigure::Server<dynamic_tutorials::TutorialsConfig> server;
-  dynamic_reconfigure::Server<dynamic_tutorials::TutorialsConfig>::CallbackType f;
+    dynamic_reconfigure::Server<uavcan_core::MotorBoardConfig> server;
+    dynamic_reconfigure::Server<uavcan_core::MotorBoardConfig>::CallbackType f;
 
-  f = boost::bind(&callback, _1, _2);
-  server.setCallback(f);
+    f = boost::bind(&callback, _1, _2);
+    server.setCallback(f);
 
-  ROS_INFO("Spinning node");
-  ros::spin();
-  return 0;
+    ROS_INFO("Spinning node");
+    ros::spin();
+    return 0;
 }
