8800f3f36ae4c85d4e083d95069e4b3392ef2c0e
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2015-10-22 00:54:58 +0200

    Split config into the PID gains & other parameters
    Use multiple servers on same node to have nested tree of parameters
    and have a nicer interface

--- uavcan_core/src/motor_config_server.cpp
+++ uavcan_core/src/motor_config_server.cpp
@@ -1,28 +1,57 @@
 #include <ros/ros.h>
 
 #include <dynamic_reconfigure/server.h>
+#include <uavcan_core/PIDConfig.h>
 #include <uavcan_core/MotorBoardConfig.h>
 
-void callback(uavcan_core::MotorBoardConfig &config, uint32_t level)
+bool callback_pos_pid(uavcan_core::PIDConfig &config, uint32_t level)
 {
-    ROS_INFO("Reconfigure Request: %.3f %.3f %.3f %.3f",
-             config.pos_p,
-             config.pos_i,
-             config.pos_d,
-             config.pos_i_limit);
+    ROS_INFO("pos pid");
+}
+
+bool callback_vel_pid(uavcan_core::PIDConfig &config, uint32_t level)
+{
+    ROS_INFO("vel pid");
+}
+
+bool callback_cur_pid(uavcan_core::PIDConfig &config, uint32_t level)
+{
+    ROS_INFO("cur pid");
+}
+
+bool callback_params(uavcan_core::MotorBoardConfig &config, uint32_t level)
+{
+    ROS_INFO("param");
 }
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "motor_config_server");
+    ros::init(argc, argv, "motor_control_config");
 
-    dynamic_reconfigure::Server<uavcan_core::MotorBoardConfig> server;
-    dynamic_reconfigure::Server<uavcan_core::MotorBoardConfig>::CallbackType f;
+    ros::NodeHandle nh_pos_pid("~/pid_position");
+    ros::NodeHandle nh_vel_pid("~/pid_velocity");
+    ros::NodeHandle nh_cur_pid("~/pid_current");
+    ros::NodeHandle nh_params("~/parameters");
 
-    f = boost::bind(&callback, _1, _2);
-    server.setCallback(f);
+    dynamic_reconfigure::Server<uavcan_core::PIDConfig> cfg_pos_pid(nh_pos_pid);
+    dynamic_reconfigure::Server<uavcan_core::PIDConfig> cfg_vel_pid(nh_vel_pid);
+    dynamic_reconfigure::Server<uavcan_core::PIDConfig> cfg_cur_pid(nh_cur_pid);
+    dynamic_reconfigure::Server<uavcan_core::MotorBoardConfig> cfg_params(nh_params);
 
-    ROS_INFO("Spinning node");
+    dynamic_reconfigure::Server<uavcan_core::PIDConfig>::CallbackType f_pos, f_vel, f_cur;
+    f_pos = boost::bind(callback_pos_pid, _1, _2);
+    f_vel = boost::bind(callback_vel_pid, _1, _2);
+    f_cur = boost::bind(callback_cur_pid, _1, _2);
+
+    dynamic_reconfigure::Server<uavcan_core::MotorBoardConfig>::CallbackType f_params;
+    f_params = boost::bind(callback_params, _1, _2);
+
+    cfg_pos_pid.setCallback(f_pos);
+    cfg_vel_pid.setCallback(f_vel);
+    cfg_cur_pid.setCallback(f_cur);
+    cfg_params.setCallback(f_params);
+
     ros::spin();
+
     return 0;
 }
