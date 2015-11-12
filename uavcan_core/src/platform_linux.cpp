ea9bb9a4aedda58e043d18f2de97ed3c5b217e7b
Antoine Albertelli <antoinea101@gmail.com>
2015-11-12 13:54:01 +0100

    Automatically choose between vcan0 and can1

--- uavcan_core/src/platform_linux.cpp
+++ uavcan_core/src/platform_linux.cpp
@@ -1,4 +1,5 @@
 #include <uavcan_linux/uavcan_linux.hpp>
+#include <ros/ros.h>
 
 uavcan::ISystemClock& getSystemClock()
 {
@@ -11,9 +12,14 @@ uavcan::ICanDriver& getCanDriver()
     static uavcan_linux::SocketCanDriver driver(dynamic_cast<const uavcan_linux::SystemClock&>(getSystemClock()));
     // Will be executed once
     if (driver.getNumIfaces() == 0) {
-        if (driver.addIface("vcan0") < 0) {
+        if (driver.addIface("can1") >= 0) {
+            ROS_INFO("Listening on CAN interface.");
+        } else if (driver.addIface("vcan0") >= 0) {
+            ROS_WARNING("Listening on virtual CAN interface.");
+        } else {
             throw std::runtime_error("Failed to add iface");
         }
     }
+
     return driver;
 }
