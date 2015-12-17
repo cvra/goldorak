b2ab5d0104cac63e4cf766c685bf73bf641291f5
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2015-12-17 01:08:33 +0100

    Make motor simulation script more accurate
    Integrate velocity correctly to compute position and encoder value of wheel

--- uavcan_core/scripts/motor_board_sim.py
+++ uavcan_core/scripts/motor_board_sim.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
+from __future__ import division
 import sys
 import math
 import rospy
 from std_msgs.msg import Float32, UInt16
 from cvra_msgs.msg import MotorControlSetpoint
 
+ENCODER_RESOLUTION = 2**16
+REFRESH_RATE = 50
+DELTA_T = 1 / REFRESH_RATE
+
 class MotorBoardSim:
     def __init__(self, motor_name):
         rospy.init_node('motor_board_sim', anonymous=True)
-        self.rate = rospy.Rate(50)
+        self.rate = rospy.Rate(REFRESH_RATE)
 
         self.vel = 0
         self.pos = 0
         self.enc = 0
-        self.vel_pub = rospy.Publisher(motor_name + '/feedback/velocity', Float32, queue_size=1)
-        self.pos_pub = rospy.Publisher(motor_name + '/feedback/position', Float32, queue_size=1)
-        self.enc_pub = rospy.Publisher(motor_name + '/feedback/encoder_raw', UInt16, queue_size=1)
+        self.vel_pub = rospy.Publisher(motor_name + '/feedback/velocity', Float32, queue_size=10)
+        self.pos_pub = rospy.Publisher(motor_name + '/feedback/position', Float32, queue_size=10)
+        self.enc_pub = rospy.Publisher(motor_name + '/feedback/encoder_raw', UInt16, queue_size=10)
 
         rospy.Subscriber(motor_name + '/setpoint', MotorControlSetpoint, self.callback)
 
@@ -24,13 +29,8 @@ class MotorBoardSim:
             self.vel = msg.velocity
 
     def update(self):
-        self.pos = self.pos + self.vel
-        self.enc = self.enc + 10 * math.floor(self.vel)
-
-        if self.enc >= 2**16:
-            self.enc = self.enc - 2**16
-        if self.enc < 0:
-            self.enc = 2**16 + self.enc
+        self.pos = self.pos + self.vel * DELTA_T
+        self.enc = (self.enc + self.vel * DELTA_T * ENCODER_RESOLUTION / math.pi) % ENCODER_RESOLUTION
 
         self.vel_pub.publish(self.vel)
         self.pos_pub.publish(self.pos)
@@ -48,4 +48,3 @@ if __name__ == '__main__':
     else:
         motor = MotorBoardSim(sys.argv[1])
         motor.run()
-
