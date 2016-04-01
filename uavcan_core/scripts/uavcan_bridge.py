92a72ac95f826bea2f9397f72dc069e2270b3666
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2016-04-01 21:56:34 +0200

    Refactor function naming

--- uavcan_core/scripts/uavcan_bridge.py
+++ uavcan_core/scripts/uavcan_bridge.py
@@ -9,29 +9,29 @@ import rospy
 
 BASE_PATH = os.path.join(os.path.dirname(__file__), '..', '..')
 
-def load_dsdl():
+def load_uavcan_msg_dsdl():
     uavcan.load_dsdl(os.path.join(BASE_PATH, 'cvra_msgs', 'uavcan', 'cvra'))
 
-def find_msg(name):
+def find_uavcan_msg(name):
     return uavcan.TYPENAMES[name]
 
-def subscribe(node, topic_name, callback_function):
-    node.add_handler(find_msg(topic_name), callback_function)
+def uavcan_subscribe(node, topic_name, callback_function):
+    node.add_handler(find_uavcan_msg(topic_name), callback_function)
 
-def fields_for_msg(msg):
+def uavcan_msg_fields(msg):
     return uavcan.get_uavcan_data_type(msg).fields
 
-def get_msg_values(msg):
-    fields = fields_for_msg(msg)
+def get_uavcan_msg_values(msg):
+    fields = uavcan_msg_fields(msg)
     result = {f.name: getattr(msg, f.name) for f in fields}
 
     return result
 
-def type_name(msg):
+def uavcan_type_name(msg):
     return uavcan.get_uavcan_data_type(msg).full_name
 
-def publish(node, type_name, data):
-    Message = find_msg(type_name)
+def uavcan_publish(node, type_name, data):
+    Message = find_uavcan_msg(type_name)
     msg = Message(**data)
 
     node.broadcast(msg)
@@ -43,19 +43,16 @@ def ros_type_from_uavcan_type(uavcan_type):
     name = "".join(s.capitalize() for s in name.split('.')[1:])
     return getattr(cvra_msgs.msg, name)
 
-def dict_from_uavcan_msg(uavcan_type, uavcan_msg):
-    msg_type = ros_type_from_uavcan_type(uavcan_type)
-    return get_msg_values(uavcan_msg)
-
 def uavcan_type_from_ros_type(ros_type):
     name = str(cvra_msgs.msg.MotorControlVelocity).split('.')[-1][:-2]
     fields = re.sub("([A-Z])", "_\\1", name).lower().lstrip("_").split("_")
     name = 'cvra.'
     name += '.'.join(fields[:-1])
     name += ''.join(['.', fields[-1].capitalize()])
-    return find_msg(name)
+    return find_uavcan_msg(name)
 
-def dict_from_ros_msg(ros_type, ros_msg):
+def get_ros_msg_values(ros_msg):
+    ros_type = ros_msg.__class__
     return {field: getattr(ros_msg, field) for field in ros_type.__slots__}
 
 
@@ -65,30 +62,30 @@ def main():
     parser.add_argument('topic')
     parser.add_argument('--interface', '-i', default='vcan0')
     parser.add_argument("--id", default=None, type=int)
-    parser.add_argument('--publish', '-p')
+    parser.add_argument('--publish', '-p', help="Publish to UAVCAN")
 
     args = parser.parse_args()
 
-    load_dsdl()
+    load_uavcan_msg_dsdl()
     node = uavcan.node.make_node(args.interface, node_id=args.id)
     rospy.init_node('uavcan_bridge', anonymous=True)
 
-    ros_type = ros_type_from_uavcan_type(find_msg(args.topic))
+    ros_type = ros_type_from_uavcan_type(find_uavcan_msg(args.topic))
     pub = rospy.Publisher('/'.join(s for s in args.topic.split('.')[1:]), ros_type, queue_size=10)
 
     def callback(transfer):
         msg = transfer.message
-        print("UAVCAN: {}({})".format(type_name(msg), get_msg_values(msg)))
-        ros_msg = ros_msg_from_uavcan_msg(find_msg(type_name(msg)), msg)
-        print("ROS: {} ({})".format(ros_type._type, ros_msg))
-        pub.publish(ros_msg)
+        data = get_uavcan_msg_values(msg)
+        print("UAVCAN: {}({})".format(uavcan_type_name(msg), data))
+        pub.publish(ros_type(**data))
+        print("ROS: {}({})".format(ros_type._type, data))
 
     if args.publish:
         data = json.loads(args.publish)
-        publish(node, args.topic, data)
+        uavcan_publish(node, args.topic, data)
         node.spin(0.5)
     else:
-        subscribe(node, args.topic, callback)
+        uavcan_subscribe(node, args.topic, callback)
         node.spin()
 
 if __name__ == '__main__':
--- uavcan_core/tests/test_uavcan_bridge.py
+++ uavcan_core/tests/test_uavcan_bridge.py
@@ -9,67 +9,63 @@ import rospy
 
 class UtilityTestCase(TestCase):
     def setUp(self):
-        uavcan_bridge.load_dsdl()
+        uavcan_bridge.load_uavcan_msg_dsdl()
 
-    def test_load_dsdl(self):
+    def test_load_uavcan_msg_dsdl(self):
         self.assertIn('cvra.Reboot', uavcan.TYPENAMES)
 
-    def test_find_msgs(self):
-        Reboot = uavcan_bridge.find_msg('cvra.Reboot')
-        StringID = uavcan_bridge.find_msg('cvra.StringID')
+    def test_find_uavcan_msgs(self):
+        Reboot = uavcan_bridge.find_uavcan_msg('cvra.Reboot')
+        StringID = uavcan_bridge.find_uavcan_msg('cvra.StringID')
 
         self.assertIsNotNone(Reboot)
         self.assertTrue(Reboot is not StringID)
 
-    def test_subscribe(self):
+    def test_uavcan_subscribe(self):
         callback = Mock()
         node = Mock()
-        uavcan_bridge.subscribe(node, 'cvra.Reboot', callback)
+        uavcan_bridge.uavcan_subscribe(node, 'cvra.Reboot', callback)
 
-        Reboot = uavcan_bridge.find_msg('cvra.Reboot')
+        Reboot = uavcan_bridge.find_uavcan_msg('cvra.Reboot')
         node.add_handler.assert_any_call(Reboot, callback)
 
-    def test_get_fields(self):
-        Reboot = uavcan_bridge.find_msg('cvra.Reboot')
-        self.assertTrue(Reboot.fields)
-
-    def test_get_fields(self):
-        Reboot = uavcan_bridge.find_msg('cvra.Reboot')
+    def test_get_uavcan_msg_fields(self):
+        Reboot = uavcan_bridge.find_uavcan_msg('cvra.Reboot')
         val = uavcan.transport.CompoundValue(Reboot)
 
-        field_names = [f.name for f in uavcan_bridge.fields_for_msg(val)]
+        field_names = [f.name for f in uavcan_bridge.uavcan_msg_fields(val)]
 
         self.assertEqual(["bootmode"], field_names)
 
     def test_get_fields_value(self):
-        Reboot = uavcan_bridge.find_msg('cvra.Reboot')
+        Reboot = uavcan_bridge.find_uavcan_msg('cvra.Reboot')
         val = uavcan.transport.CompoundValue(Reboot, bootmode=1)
 
         self.assertEqual(val.bootmode, 1)
 
-    def test_get_fields_dict(self):
-        Reboot = uavcan_bridge.find_msg('cvra.Reboot')
+    def test_get_uavcan_fields_dict(self):
+        Reboot = uavcan_bridge.find_uavcan_msg('cvra.Reboot')
         val = uavcan.transport.CompoundValue(Reboot, bootmode=1)
 
         expected = {'bootmode':1}
-        self.assertEqual(expected, uavcan_bridge.get_msg_values(val))
+        self.assertEqual(expected, uavcan_bridge.get_uavcan_msg_values(val))
 
-    def test_get_type_name(self):
-        Reboot = uavcan_bridge.find_msg('cvra.Reboot')
+    def test_get_uavcan_type_name(self):
+        Reboot = uavcan_bridge.find_uavcan_msg('cvra.Reboot')
         val = uavcan.transport.CompoundValue(Reboot, bootmode=1)
 
         expected = 'cvra.Reboot'
-        self.assertEqual(uavcan_bridge.type_name(val), expected)
+        self.assertEqual(uavcan_bridge.uavcan_type_name(val), expected)
 
 class PublisherTestCase(TestCase):
     def setUp(self):
-        uavcan_bridge.load_dsdl()
+        uavcan_bridge.load_uavcan_msg_dsdl()
 
-    def test_publish(self):
+    def test_uavcan_publish(self):
         node = Mock()
-        uavcan_bridge.publish(node, 'cvra.Reboot', {'bootmode': 1})
+        uavcan_bridge.uavcan_publish(node, 'cvra.Reboot', {'bootmode': 1})
 
-        Reboot = uavcan_bridge.find_msg('cvra.Reboot')
+        Reboot = uavcan_bridge.find_uavcan_msg('cvra.Reboot')
 
         msg = node.broadcast.call_args[0][0]
 
@@ -77,36 +73,36 @@ class PublisherTestCase(TestCase):
 
 class RosFromUavcanTestCase(TestCase):
     def setUp(self):
-        uavcan_bridge.load_dsdl()
+        uavcan_bridge.load_uavcan_msg_dsdl()
 
     def test_ros_type_from_uavcan_type(self):
-        uavcan_type = uavcan_bridge.find_msg("cvra.motor.control.Velocity")
+        uavcan_type = uavcan_bridge.find_uavcan_msg("cvra.motor.control.Velocity")
         expected = cvra_msgs.msg.MotorControlVelocity
         actual = uavcan_bridge.ros_type_from_uavcan_type(uavcan_type)
 
         self.assertEqual(actual, expected)
 
-    def test_dict_from_uavcan_msg(self):
-        VelocityMsg = uavcan_bridge.find_msg('cvra.motor.control.Velocity')
+    def test_get_uavcan_msg_values(self):
+        VelocityMsg = uavcan_bridge.find_uavcan_msg('cvra.motor.control.Velocity')
         val = uavcan.transport.CompoundValue(VelocityMsg, node_id=42, velocity=3)
 
         expected = {'node_id':42, 'velocity':3}
-        actual = uavcan_bridge.dict_from_uavcan_msg(VelocityMsg, val)
+        actual = uavcan_bridge.get_uavcan_msg_values(val)
 
         self.assertEqual(actual, expected)
 
     def test_uavcan_type_from_ros_type(self):
         ros_type = cvra_msgs.msg.MotorControlVelocity
-        expected = uavcan_bridge.find_msg("cvra.motor.control.Velocity")
+        expected = uavcan_bridge.find_uavcan_msg("cvra.motor.control.Velocity")
         actual = uavcan_bridge.uavcan_type_from_ros_type(ros_type)
 
         self.assertEqual(actual, expected)
 
-    def test_dict_from_ros_msg(self):
+    def test_get_ros_msg_values(self):
         val = cvra_msgs.msg.MotorControlVelocity(node_id=42, velocity=3)
 
-        VelocityMsg = uavcan_bridge.find_msg('cvra.motor.control.Velocity')
+        VelocityMsg = uavcan_bridge.find_uavcan_msg('cvra.motor.control.Velocity')
         expected = {'node_id':42, 'velocity':3}
-        actual = uavcan_bridge.dict_from_ros_msg(cvra_msgs.msg.MotorControlVelocity, val)
+        actual = uavcan_bridge.get_ros_msg_values(val)
 
         self.assertEqual(actual, expected)
