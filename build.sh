825ee4bd74ea7818582fe666684c7ecbe00953ee
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2016-05-17 13:36:05 +0200

    Fix build on fresh install

--- build.sh
+++ build.sh
@@ -24,6 +24,8 @@ popd
 popd # exit uavcan_core
 
 pushd ../..
-catkin build cvra_msgs uavcan_core goldorak_diffbase goldorak_bringup goldorak_state_publisher goldorak_description goldorak_navigation goldorak_strategy
+catkin build cvra_msgs
+source devel/setup.bash
+catkin build uavcan_core goldorak_diffbase goldorak_bringup goldorak_state_publisher goldorak_description goldorak_navigation goldorak_strategy
 source devel/setup.bash
 popd
