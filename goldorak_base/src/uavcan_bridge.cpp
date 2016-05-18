dd15a2d57a2f5c636b0df2c1d36d80de04141cd0
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2016-05-19 00:33:40 +0200

    Switch UAVCAN bridge back to isolated node
    There were problems when running the bridge in a thread.
    The main problem being related to tx queue length

---
+++ goldorak_base/src/uavcan_bridge.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+
+#include <uavcan/uavcan.hpp>
+#include <uavcan/protocol/debug/LogMessage.hpp>
+
+#include "ros/ros.h"
+#include "UavcanRosMotorController.hpp"
+#include "UavcanRosProximityBeaconDriver.hpp"
+
+extern uavcan::ICanDriver& getCanDriver();
+extern uavcan::ISystemClock& getSystemClock();
+
+
+class UavcanRosBridge
+{
+    static const unsigned NodeMemoryPoolSize = 16384;
+    typedef uavcan::Node<NodeMemoryPoolSize> Node;
+
+public:
+    int uavcan_id;
+    Node uavcan_node;
+    ros::NodeHandle ros_node;
+
+    UavcanRosMotorController motor_controller;
+    UavcanRosProximityBeaconDriver beacon_driver;
+
+    uavcan::Subscriber<uavcan::protocol::debug::LogMessage> uavcan_log_sub;
+
+    UavcanRosBridge(int id):
+        uavcan_node(getCanDriver(), getSystemClock()),
+        uavcan_log_sub(this->uavcan_node),
+        motor_controller(this->uavcan_node, this->ros_node),
+        beacon_driver(this->uavcan_node, this->ros_node)
+    {
+        this->uavcan_id = id;
+
+        /* Start UAVCAN node and publisher */
+        const int self_node_id = this->uavcan_id;
+        this->uavcan_node.setNodeID(self_node_id);
+        this->uavcan_node.setName("uavcan_ros_bridge");
+
+        const int node_start_res = uavcan_node.start();
+        if (node_start_res < 0) {
+            throw std::runtime_error("Failed to start the UAVCAN bridge node");
+        }
+
+        this->uavcan_log_sub.start(
+            [&](const uavcan::ReceivedDataStructure<uavcan::protocol::debug::LogMessage>& msg)
+            {
+                std::cout << msg << std::endl;
+            }
+        );
+
+        this->uavcan_node.setModeOperational();
+    }
+
+    void spin(void)
+    {
+        while (ros::ok()) {
+            ros::spinOnce();
+            const int res = this->uavcan_node.spin(uavcan::MonotonicDuration::fromMSec(1));
+            if (res < 0) {
+                std::cerr << "Transient failure in UAVCAN bridge: " << res << std::endl;
+            }
+        }
+    }
+};
+
+
+int main(int argc, const char** argv)
+{
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <uavcan-id>" << std::endl;
+        return 1;
+    }
+
+    ros::init(argc, (char **)argv, "uavcan_bridge");
+
+    UavcanRosBridge uavcan_bridge(std::stoi(argv[1]));
+
+    uavcan_bridge.spin();
+
+    return 0;
+}
